3d7cf7ebfc7f672a87137e6a9bf5fb50601e372e
PureDeep <542057950@qq.com>
2022-08-12 00:17:55 +0800

    Update ResolveContact and momentum impulse

--- code/Physics/Contact.cpp
+++ code/Physics/Contact.cpp
@@ -13,8 +13,17 @@ void ResolveContact(contact_t& contact)
     Body* body_a = contact.bodyA;
     Body* body_b = contact.bodyB;
 
-    body_a->m_linearVelocity.Zero();
-    body_b->m_linearVelocity.Zero();
+    const float inv_mass_a = body_a->m_inverseMass;
+    const float inv_mass_b = body_b->m_inverseMass;
+
+    // 计算碰撞处的法线向量
+    const Vec3& n = contact.normal;
+    const Vec3 v_ab = body_a->m_linearVelocity - body_b->m_linearVelocity;
+    const float impulse_j = -2.0f * v_ab.Dot(n) / (inv_mass_a + inv_mass_b); // v_ab.Dot(n)得到相对速度沿碰撞接触点法向的分量
+    const Vec3 vector_impulse_j = n * impulse_j;
+
+    body_a->ApplyImpulseLinear(vector_impulse_j * 1.0f);
+    body_b->ApplyImpulseLinear(vector_impulse_j * -1.0f);
 
     // Distance project . 阻止相撞的两个物体相互穿透
     const float t_a = body_a->m_inverseMass / (body_a->m_inverseMass + body_b->m_inverseMass);
