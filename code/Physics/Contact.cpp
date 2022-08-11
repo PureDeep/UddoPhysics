b80ca29d816b8559b8014e1731810cec7696c8a5
PureDeep <542057950@qq.com>
2022-08-11 23:46:53 +0800

    Update Intersections.cpp

--- code/Physics/Contact.cpp
+++ code/Physics/Contact.cpp
@@ -15,4 +15,12 @@ void ResolveContact(contact_t& contact)
 
     body_a->m_linearVelocity.Zero();
     body_b->m_linearVelocity.Zero();
+
+    // Distance project . 阻止相撞的两个物体相互穿透
+    const float t_a = body_a->m_inverseMass / (body_a->m_inverseMass + body_b->m_inverseMass);
+    const float t_b = body_b->m_inverseMass / (body_a->m_inverseMass + body_b->m_inverseMass);
+
+    const Vec3 ds = contact.ptOnB_WorldSpace - contact.ptOnA_WorldSpace;
+    body_a->m_position += ds * t_a;
+    body_b->m_position -= ds * t_b;
 }
--- code/Physics/Intersections.cpp
+++ code/Physics/Intersections.cpp
@@ -22,7 +22,7 @@ bool Intersect(Body* bodyA, Body* bodyB, contact_t& contact)
     const ShapeSphere* shpere_b = dynamic_cast<ShapeSphere*>(bodyB->m_shape);
 
     contact.ptOnA_WorldSpace = bodyA->m_position + contact.normal * shpere_a->m_radius;
-    contact.ptOnB_WorldSpace = bodyB->m_position + contact.normal * shpere_b->m_radius;
+    contact.ptOnB_WorldSpace = bodyB->m_position - contact.normal * shpere_b->m_radius;
 
     const float radius_ab = shpere_a->m_radius + shpere_b->m_radius;
     const float length_square = ab.GetLengthSqr();
@@ -53,7 +53,7 @@ bool Intersect(Body* bodyA, Body* bodyB, const float dt, contact_t& contact)
  */
 bool Intersect(const Body* body_a, const Body* body_b)
 {
-    const Vec3 distance_ab = body_a->m_position - body_b->m_position;
+    const Vec3 distance_ab = body_b->m_position - body_a->m_position;
     const auto sphere_a = dynamic_cast<const ShapeSphere*>(body_a->m_shape);
     const auto sphere_b = dynamic_cast<const ShapeSphere*>(body_b->m_shape);
 
